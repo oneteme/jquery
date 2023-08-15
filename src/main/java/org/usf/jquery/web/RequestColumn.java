0500022a53638afc159d304eee5ca8df9fd5f476
u$f <usf.alami@gmail.com>
2023-08-15 23:52:33 +0200

    parse-v2

--- src/main/java/org/usf/jquery/web/RequestColumn.java
+++ src/main/java/org/usf/jquery/web/RequestColumn.java
@@ -2,15 +2,20 @@ package org.usf.jquery.web;
 
 import static java.lang.String.join;
 import static java.util.Arrays.copyOfRange;
+import static java.util.Collections.emptyList;
+import static java.util.Objects.isNull;
 import static java.util.Optional.ofNullable;
 import static java.util.regex.Pattern.compile;
 import static org.usf.jquery.core.DBColumn.count;
 import static org.usf.jquery.core.DBFunction.lookup;
 import static org.usf.jquery.core.Utils.AUTO_TYPE;
 import static org.usf.jquery.core.Utils.isBlank;
+import static org.usf.jquery.core.Utils.isEmpty;
 import static org.usf.jquery.core.Validation.VARIABLE_PATTERN;
+import static org.usf.jquery.core.Validation.requireLegalVariable;
 import static org.usf.jquery.web.JQueryContext.context;
 import static org.usf.jquery.web.ParseException.cannotEvaluateException;
+import static org.usf.jquery.web.RequestColumn.RequestEntry.ressource;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -20,7 +25,9 @@ import org.usf.jquery.core.DBColumn;
 import org.usf.jquery.core.TypedFunction;
 
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 /**
  * 
@@ -147,5 +154,102 @@ public final class RequestColumn {
 			}
 		};
 	}
-    
+	
+	private static List<ResourceList> parseRequestColumn(String s) {
+		if(isBlank(s)) {
+			return emptyList();
+		}
+		var res = new LinkedList<ResourceList>();
+		res.add(new ResourceList());
+		int from = 0;
+		int to = 0;
+		char c = 0;
+		for(;;) {
+			while(to < s.length() && legalVariableChar(c = s.charAt(to))) to++;
+			res.getLast().add(ressource(s.substring(from, to)));
+			if(to == s.length()) {
+				break;
+			}
+			if(c == '(') {
+				var jmp = s.indexOf(')', ++to);
+				if(jmp > -1) {
+					var nest = s.indexOf('(', to);
+					if(nest > -1 && nest < jmp) { //avoid (..(..)
+						throw new IllegalArgumentException("'(' not allowed at index=" + nest);
+					}
+					res.getLast().getList().getLast().setArgs(s.substring(to, jmp).split(",")); //split later !?
+				}
+				else {
+					throw new IllegalArgumentException("')' expected after index=" + jmp);	
+				}
+				if((to = ++jmp) == s.length()) {
+					break;
+				}
+				c = s.charAt(to);
+			}
+			if(c == ':') {
+				var jmp = s.indexOf(',', to+1);
+				if(jmp == -1) {
+					jmp = s.length();
+				}
+				res.getLast().setTag(s.substring(to, jmp));
+				if((to = jmp) == s.length()) {
+					break;
+				}
+				c = s.charAt(to);
+			}
+			if(c == ',') {
+				res.add(new ResourceList());
+			}
+			else if(c != '.') {
+				throw new IllegalArgumentException("'" + s.charAt(to) + "' not valid at index=" + to);
+			}
+			if((from = ++to) == s.length()) {
+				throw new IllegalArgumentException("'" + s.charAt(to-1) + "' not allowed at the end");
+			}
+		}
+		System.out.println(s+ " => " + res);
+		return res;
+	}
+	
+	private static boolean legalVariableChar(char c) {
+		return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z' ) || (c >= '0' && c <= '9') || c == '_';
+	}
+	
+	
+	@Setter
+	@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+	static final class RequestEntry {
+		
+		private final String name;
+		private String[] args;
+		
+		public static RequestEntry ressource(String name) {
+			return new RequestEntry(requireLegalVariable(name));
+		}
+		
+		@Override
+		public String toString() {
+			return isEmpty(args) ? name : name + "(" + join(", ", args) + ")"; 
+		}
+	}
+
+	@Setter
+	@Getter
+	static final class ResourceList {
+		
+		private String tag;
+		private final LinkedList<RequestEntry> list = new LinkedList<>();
+
+		public void add(RequestEntry res) {
+			list.add(res);
+		}
+		
+		@Override
+		public String toString() {
+			return isNull(tag) ? list.toString() : list.toString() + ":" + tag;
+		}
+		
+	}
+	
 }
