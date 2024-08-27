0156a2769d39e2faf46633a1fc0c0cb330daeff2
u$f <usf.alami@gmail.com>
2024-08-27 17:22:48 +0200

    edit

---
+++ src/main/java/org/usf/jquery/web/PartitionBuilder.java
@@ -0,0 +1,15 @@
+package org.usf.jquery.web;
+
+import org.usf.jquery.core.Partition;
+
+/**
+ * 
+ * @author u$f
+ *
+ */
+@FunctionalInterface
+public interface PartitionBuilder {
+
+	Partition build();
+	
+}
