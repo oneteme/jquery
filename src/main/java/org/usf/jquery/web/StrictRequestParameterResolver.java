995c64aec70aad8824b8bba5c7ddfb6a8ce795f9
TARHOUNI Mohamed Amine <mohamedamine.tarhouni@ynov.com>
2025-05-23 18:51:15 +0200

    test_StrictRequestQueryParam

--- src/main/java/org/usf/jquery/web/StrictRequestParameterResolver.java
+++ src/main/java/org/usf/jquery/web/StrictRequestParameterResolver.java
@@ -15,7 +15,6 @@ import static org.usf.jquery.web.Parameters.LIMIT;
 import static org.usf.jquery.web.Parameters.OFFSET;
 import static org.usf.jquery.web.Parameters.ORDER;
 
-import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
@@ -33,15 +32,16 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 @RequiredArgsConstructor
-public final class RequestParameterResolver2 {// spring connection bridge
-	
+public final class StrictRequestParameterResolver {// spring connection bridge
+
 	private static final Set<String> KEYWORDS = Set.of(COLUMN, DISTINCT, JOIN, OFFSET, LIMIT, ORDER);
 
-	public QueryComposer requestQueryCheck(@NonNull RequestQueryParam2 ant, @NonNull Map<String, String[]> parameterMap) {
+	public QueryComposer requestQueryCheck(@NonNull RequestQueryParam2 ant,
+			@NonNull Map<String, String[]> parameterMap) {
 		var t = currentTimeMillis();
 		log.trace("parsing request...");
-		for(var s : KEYWORDS) {
-			illegalArgumentIf(parameterMap.containsKey(s), ()-> s + " argument not allowed");
+		for (var s : KEYWORDS) {
+			illegalArgumentIf(parameterMap.containsKey(s), () -> s + " argument not allowed");
 		}
 		var mutableMap = new LinkedHashMap<>(parameterMap); // modifiable map + preserve order
 		appendParam(mutableMap, COLUMN, ant.column());
@@ -50,41 +50,43 @@ public final class RequestParameterResolver2 {// spring connection bridge
 		appendParam(mutableMap, ORDER, ant.order());
 		appendParam(mutableMap, LIMIT, ant.limit());
 		appendParam(mutableMap, OFFSET, ant.offset());
-		if(!isEmpty(ant.filters())) {
-			for (String filter : ant.filters()) {
-				var splittedFilter = filter.split("=", 2);
-				illegalArgumentIf(splittedFilter[0].length()<=0 || splittedFilter[0].contains("=") || (splittedFilter.length>1 && splittedFilter[1].contains("=")), ()-> "Incorrect filter format => " + filter);
-				generateFilterField(mutableMap, splittedFilter);
-			} 
-		}
+		appendParams(parameterMap, ant.filters());
 		if (!isEmpty(ant.ignoreParameters())) {
 			for (var k : ant.ignoreParameters()) {
 				mutableMap.remove(k);
 			}
 		}
 		var env = ant.database().isEmpty() ? defaultEnvironment() : getEnvironment(ant.database());
-		return exec(env, e-> getRequestParser().parse(e, ant.view(), ant.variables(), mutableMap));
+		return exec(env, e -> getRequestParser().parse(e, ant.view(), ant.variables(), mutableMap));
 	}
-	
-	private void appendParam(Map<String, String[]> params, String key, int value) {
-		if(value > -1) {
-			params.put(key, new String[] {valueOf(value)});
-		}
-	}
-	
-	private void appendParam(Map<String, String[]> params, String key, boolean value) {
-		if(value) {
-			params.put(key, new String[] {valueOf(value)});
+
+	void appendParam(Map<String, String[]> params, String key, int value) {
+		if (value > -1) {
+			params.put(key, new String[] { valueOf(value) });
 		}
 	}
 
-	private void appendParam(Map<String, String[]> params, String key, String[] value) {
-		if(!isEmpty(value)) {
+	void appendParam(Map<String, String[]> params, String key, boolean value) {
+		if (value) {
+			params.put(key, new String[] { valueOf(value) });
+		}
+	}
+
+	void appendParam(Map<String, String[]> params, String key, String[] value) {
+		if (!isEmpty(value)) {
 			params.put(key, value);
 		}
 	}
-	private void generateFilterField(Map<String, String[]> params,String[] arr) {
-		String filterValue = arr.length>1 && arr[1].length() > 0 ? arr[1] : "";
-		params.put(arr[0],new String[] {filterValue});
+
+	void appendParams(Map<String, String[]> params, String[] keys) {
+		if (!isEmpty(keys)) {
+			for(var filter : keys) {
+				illegalArgumentIf(filter.chars().filter(v -> v == '=').count() > 1,
+						()-> "incorrect filter format : '" + filter + "'");
+				var arr = filter.split("=");
+				illegalArgumentIf(arr[0].isEmpty(), ()-> "filter key cannot be null : '" + filter + "'");
+				params.put(arr[0], arr.length > 1 ? new String[] { arr[1] } : null);
+			}
+		}
 	}
 }
